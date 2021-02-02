47082a61a1bb5b9d90a4dcde4760d70f70f61a74
Corentin Martin <mrt.corentin44@gmail.com>
2021-02-02 16:27:26 +0100

    footer proto update

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,54 +1,59 @@
 <script defer src="assets/js/app.js"></script>
 
 
-<div class='container-fluid'>
-    <div class="card">
-        <div class="card-body">
-            <div class="row m-0 pt-5">
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"><i class="fas fa-hand-holding-usd fa-3x"></i>
-                        <div class="card-title"> Mentions Légales</div>
-                        <a href="#"> Politique de confidentialité </a>
-                        <a href="#"> Conditions générales d'utilisation </a>
-                        <a href="#"> Utilisation des cookies </a>
-                    </div>
-                </div>
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"><i class="far fa-handshake fa-3x"></i>
 
-                    </div>
-                </div>
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"><i class="fas fa-mobile-alt fa-3x"></i>
-                        <div class="card-title"> Plan du Site</div>
-                        <a href='#'>Accueil</a>
-                        <a href='#'>Sécurité</a>
-                        <a href='#'>Partenaires</a>
-                        <a href='#'>Nous contacter</a>
-                        <a href='#'>Mon Profil</a>
-                    </div>
-                </div>
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"><i class="fas fa-user-secret fa-3x"></i>
-                        <div class="card-title"> STAY<br/> SECURE</div>
-                        <p><small>Save customers' cards for future phone orders</small></p>
-                        <div class="learn-more">
-                            <p><small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-contact">
-            <span><i class="phone"></i>02.23.20.29.13</span>
-            <div>
-                <span class="sm-text">Restez au courant de nos actualités :</span>
-                <ul class="footer-right">
-                    <li><img src="assets/svg/icon-facebook.svg" height="35" width="35" alt="icon facebook"/></li>
-                    <li><img src="assets/svg/icon-insta.svg" height="35" width="35" alt="icon insta"/></li>
-                    <li><img src="assets/svg/icon-linkedin.svg" height="35" width="35" alt="icon linkedin"/></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+<div class="container">
+
+    <span>Copyright © 2021 WIS 1 / Tous droits réservés</span>
+
+    <a href="#"> Mentions Légales </a>
+
+    <ul class="footer-right">
+        <li><img src="assets/svg/icon-facebook.svg" height="35" width="35" alt="icon facebook"/></li>
+        <li><img src="assets/svg/icon-insta.svg" height="35" width="35" alt="icon insta"/></li>
+        <li><img src="assets/svg/icon-linkedin.svg" height="35" width="35" alt="icon linkedin"/></li>
+    </ul>
+
+</div>
+
+<div>
+    <ul>
+        <li>
+            <span>Plan du site</span>
+        </li>
+        <li>
+            <a href="#">Accueil</a>
+        </li>
+        <li>
+            <a href="#">Sécurité</a>
+        </li>
+        <li>
+            <a href="#">Nos partenaires</a>
+        </li>
+        <li>
+            <a href="#">Mon profil</a>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            <span>Mentions Légales</span>
+        </li>
+        <li>
+            <a href="#">Politique de confidentialité</a>
+        </li>
+        <li>
+            <a href="#">Conditions générales d'utilisation</a>
+        </li>
+        <li>
+            <a href="#">Utilisation des cookies</a>
+        </li>
+    </ul>
+
+    <ul>
+        <li><img src="assets/svg/icon-facebook.svg" height="35" width="35" alt="icon facebook"/></li>
+        <li><img src="assets/svg/icon-insta.svg" height="35" width="35" alt="icon insta"/></li>
+        <li><img src="assets/svg/icon-linkedin.svg" height="35" width="35" alt="icon linkedin"/></li>
+    </ul>
+
 </div>
