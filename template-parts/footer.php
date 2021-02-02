62f632054913ca67efc730af90309538d0c14c96
Corentin Martin <mrt.corentin44@gmail.com>
2021-02-02 12:09:33 +0100

    footer update 2

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -9,16 +9,14 @@
                 <div class="card col-12 col-md-3">
                     <div class="card-content"> <i class="fas fa-hand-holding-usd fa-3x"></i>
                         <div class="card-title"> Mentions Légales </div>
-                        <a href="#" >Politique de confidentialité</></a>
-                        <a href="#" >Conditions générales d'utilisation</></a>
-                        <a href="#" >Utilisation des cookies</></a>
+                        <a href="#" > Politique de confidentialité </a>
+                        <a href="#" > Conditions générales d'utilisation </a>
+                        <a href="#" > Utilisation des cookies </a>
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
                     <div class="card-content"> <i class="far fa-handshake fa-3x"></i>
-                        <img src="assets/svg/icon%20facebook.svg" height="1000re6" width="16" alt="icon facebook"/>
-                        <img src="assets/svg/icon%20insta.svg" height="16" width="16" alt="icon insta"/>
-                        <img src="assets/svg/icon%20linkedin.svg" height="16" width="16" alt="icon linkedin"/>
+
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
@@ -44,7 +42,10 @@
         <div class="card-footer row m-0">
             <p> <label> <i class="fas fa-phone fa-rotate-90 text-primary"></i> </label> 800-601-0230</p>
             <div>
-                <p> <small class="follow-text">FOLLOW US ON SOCIAL MEDIA</small> <label class="footer-right"> <i class="fab fa-instagram"></i> <i class="fab fa-facebook-square"></i> <i class="fab fa-linkedin"></i> <i class="fab fa-twitter-square"></i> </label> </p>
+                <p> <small class="follow-text">FOLLOW US ON SOCIAL MEDIA</small> <label class="footer-right">
+                        <img src="assets/svg/icon-facebook.svg" height="16" width="16" alt="icon facebook"/>
+                        <img src="assets/svg/icon-insta.svg" height="16" width="16" alt="icon insta"/>
+                        <img src="assets/svg/icon-linkedin.svg" height="16" width="16" alt="icon linkedin"/>
             </div>
         </div>
     </div>
