1bfbf5e0861aaf87dc022f6dc0be3805f52738f8
Corentin Martin <mrt.corentin44@gmail.com>
2021-02-02 12:41:56 +0100

    footer update 3

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -21,8 +21,12 @@
                 </div>
                 <div class="card col-12 col-md-3">
                     <div class="card-content"> <i class="fas fa-mobile-alt fa-3x"></i>
-                        <div class="card-title"> STREAMLINE PHONE PAYMENTS </div>
-                        <p><small>Save customers' cards for future phone orders</small></p>
+                        <div class="card-title"> Plan du Site </div>
+                       <a href='#'>Accueil</ahref>
+                       <a href='#'>Sécurité</ahref>
+                       <a href='#'>Partenaires</ahref>
+                       <a href='#'>Nous contacter</ahref>
+                       <a href='>Mon Profil</ahref>
                         <div class="learn-more">
                             <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
                         </div>
@@ -40,12 +44,12 @@
             </div>
         </div>
         <div class="card-footer row m-0">
-            <p> <label> <i class="fas fa-phone fa-rotate-90 text-primary"></i> </label> 800-601-0230</p>
+            <p> <label> <i class="fas fa-phone fa-rotate-90 text-primary"></i> </label> 02.23.20.29.13</p>
             <div>
                 <p> <small class="follow-text">FOLLOW US ON SOCIAL MEDIA</small> <label class="footer-right">
-                        <img src="assets/svg/icon-facebook.svg" height="16" width="16" alt="icon facebook"/>
-                        <img src="assets/svg/icon-insta.svg" height="16" width="16" alt="icon insta"/>
-                        <img src="assets/svg/icon-linkedin.svg" height="16" width="16" alt="icon linkedin"/>
+                        <img src="assets/svg/icon-facebook.svg" height="35" width="35" alt="icon facebook"/>
+                        <img src="assets/svg/icon-insta.svg" height="35" width="35" alt="icon insta"/>
+                        <img src="assets/svg/icon-linkedin.svg" height="35" width="35" alt="icon linkedin"/>
             </div>
         </div>
     </div>
