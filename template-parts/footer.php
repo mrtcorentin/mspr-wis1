5828990d5ba6b4862249334885a05144773cd27c
Corentin Martin <mrt.corentin44@gmail.com>
2021-02-02 14:22:37 +0100

    footer proto 1

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,4 +1,3 @@
-
 <script defer src="assets/js/app.js"></script>
 
 
@@ -7,49 +6,48 @@
         <div class="card-body">
             <div class="row m-0 pt-5">
                 <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="fas fa-hand-holding-usd fa-3x"></i>
-                        <div class="card-title"> Mentions Légales </div>
-                        <a href="#" > Politique de confidentialité </a>
-                        <a href="#" > Conditions générales d'utilisation </a>
-                        <a href="#" > Utilisation des cookies </a>
+                    <div class="card-content"><i class="fas fa-hand-holding-usd fa-3x"></i>
+                        <div class="card-title"> Mentions Légales</div>
+                        <a href="#"> Politique de confidentialité </a>
+                        <a href="#"> Conditions générales d'utilisation </a>
+                        <a href="#"> Utilisation des cookies </a>
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="far fa-handshake fa-3x"></i>
+                    <div class="card-content"><i class="far fa-handshake fa-3x"></i>
 
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="fas fa-mobile-alt fa-3x"></i>
-                        <div class="card-title"> Plan du Site </div>
-                       <a href='#'>Accueil</ahref>
-                       <a href='#'>Sécurité</ahref>
-                       <a href='#'>Partenaires</ahref>
-                       <a href='#'>Nous contacter</ahref>
-                       <a href='>Mon Profil</ahref>
-                        <div class="learn-more">
-                            <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
-                        </div>
+                    <div class="card-content"><i class="fas fa-mobile-alt fa-3x"></i>
+                        <div class="card-title"> Plan du Site</div>
+                        <a href='#'>Accueil</a>
+                        <a href='#'>Sécurité</a>
+                        <a href='#'>Partenaires</a>
+                        <a href='#'>Nous contacter</a>
+                        <a href='#'>Mon Profil</a>
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="fas fa-user-secret fa-3x"></i>
-                        <div class="card-title"> STAY<br /> SECURE </div>
+                    <div class="card-content"><i class="fas fa-user-secret fa-3x"></i>
+                        <div class="card-title"> STAY<br/> SECURE</div>
                         <p><small>Save customers' cards for future phone orders</small></p>
                         <div class="learn-more">
-                            <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
+                            <p><small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small></p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="card-footer row m-0">
-            <p> <label> <i class="fas fa-phone fa-rotate-90 text-primary"></i> </label> 02.23.20.29.13</p>
+        <div class="footer-contact">
+            <span><i class="phone"></i>02.23.20.29.13</span>
             <div>
-                <p> <small class="follow-text">FOLLOW US ON SOCIAL MEDIA</small> <label class="footer-right">
-                        <img src="assets/svg/icon-facebook.svg" height="35" width="35" alt="icon facebook"/>
-                        <img src="assets/svg/icon-insta.svg" height="35" width="35" alt="icon insta"/>
-                        <img src="assets/svg/icon-linkedin.svg" height="35" width="35" alt="icon linkedin"/>
+                <span class="sm-text">Restez au courant de nos actualités :</span>
+                <ul class="footer-right">
+                    <li><img src="assets/svg/icon-facebook.svg" height="35" width="35" alt="icon facebook"/></li>
+                    <li><img src="assets/svg/icon-insta.svg" height="35" width="35" alt="icon insta"/></li>
+                    <li><img src="assets/svg/icon-linkedin.svg" height="35" width="35" alt="icon linkedin"/></li>
+                </ul>
             </div>
         </div>
     </div>
