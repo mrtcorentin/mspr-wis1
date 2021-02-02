f0cbe3aefe068d40bacdc7db04b5353c4d490947
Cabus <coline.cabuspro@gmail.com>
2021-02-02 21:17:56 +0100

    Informations update

---
+++ notre-concept.php
@@ -0,0 +1,120 @@
+<?php
+
+$page = [
+    'title' => 'Notre concept',
+    'description' => 'La description de la page',
+];
+
+require_once 'template-parts/header.php'; ?>
+
+<main id="main">
+
+
+    <section id="jumbotron-1">
+
+
+        <div class="container">
+
+            <h1 class="h1 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+
+            <div class="row align-items-start">
+
+                <div class="col">
+
+                    <img src="assets/svg/shopping2.svg" class="img-thumbnail">
+
+                </div>
+
+                <div class="col">
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolore esse facere harum
+                        inventore pariatur recusandae rem tempora, veritatis voluptates? Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Atque dolore esse facere harum
+                        inventore pariatur recusandae rem tempora, veritatis voluptates?</p>
+
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, sint.</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur debitis dicta enim ex
+                        iure pariatur qui quisquam quod reprehenderit? Dolore, ducimus eum inventore iure maiores
+                        necessitatibus repudiandae sed voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Deserunt dolorem iure placeat repellendus, sint soluta?</p>
+
+                    <div class="button-2">
+                        <button type="button" class="btn btn-black">Secondary</button>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div class="container">
+
+                <h1 class="h1 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                <div class="row align-items-start">
+                    <div class="col">
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolore esse facere harum
+                            inventore pariatur recusandae rem tempora, veritatis voluptates? Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit. Atque dolore esse facere harum
+                            inventore pariatur recusandae rem tempora, veritatis voluptates?</p>
+
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, sint.</h3>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur debitis dicta enim ex
+                            iure pariatur qui quisquam quod reprehenderit? Dolore, ducimus eum inventore iure maiores
+                            necessitatibus repudiandae sed voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Deserunt dolorem iure placeat repellendus, sint soluta?</p>
+
+                        <div class="button-2">
+                            <button type="button" class="btn btn-black">Secondary</button>
+                        </div>
+                    </div>
+
+
+                    <div class="col">
+
+                        <img src="assets/svg/help2.svg" class="img-thumbnail">
+
+                    </div>
+                </div>
+
+
+
+
+                <div class="container">
+
+                    <h1 class="h1 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+
+                    <div class="row align-items-start">
+
+                        <div class="col">
+
+                            <img src="assets/svg/help.svg" class="img-thumbnail">
+
+                        </div>
+
+                        <div class="col">
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolore esse facere harum
+                                inventore pariatur recusandae rem tempora, veritatis voluptates? Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit. Atque dolore esse facere harum
+                                inventore pariatur recusandae rem tempora, veritatis voluptates?</p>
+
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, sint.</h3>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur debitis dicta enim ex
+                                iure pariatur qui quisquam quod reprehenderit? Dolore, ducimus eum inventore iure maiores
+                                necessitatibus repudiandae sed voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit. Deserunt dolorem iure placeat repellendus, sint soluta?</p>
+
+                            <div class="button-2">
+                                <button type="button" class="btn btn-black">Secondary</button>
+                            </div>
+                        </div>
+                    </div>
+    </section>
+</main>
+
+
+<?php require_once 'template-parts/footer.php'; ?>
