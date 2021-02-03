83b7f85afde2913a39477659c782f111c774c59d
Corentin Martin <mrt.corentin44@gmail.com>
2021-02-03 12:47:07 +0100

    Update buttons & cards

--- template-parts/header.php
+++ template-parts/header.php
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
 
-            <img src="assets/images/logo.png" alt="#" width="40" height="55">
+            <a href="home.php"> <img src="assets/images/logo.png" alt="#" width="40" height="55"></a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
                     aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,13 +14,13 @@
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item me-4">
-                        <a class="nav-link" href="index.php">Accueil</a>
+                        <a class="nav-link" href="./home.php">Accueil</a>
                     </li>
                     <li class="nav-item me-4">
-                        <a class="nav-link" href="notre-concept.php">Notre concept</a>
+                        <a class="nav-link" href="./notre-concept.php">Notre concept</a>
                     </li>
                     <li class="nav-item me-4">
-                        <a class="nav-link" href="security.php">Sécurité</a>
+                        <a class="nav-link" href="./security.php">Sécurité</a>
                     </li>
                     <li class="nav-item me-4">
                         <a class="nav-link" href="#">Partenaires</a>
