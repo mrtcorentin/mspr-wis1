293c0df8bad5553f681be3ecbaea60626129b6a5
Cabus <coline.cabuspro@gmail.com>
2021-02-04 11:05:05 +0100

    Auto stash before merge of "main" and "origin/main"

--- security.php
+++ security.php
@@ -27,13 +27,189 @@ require_once 'template-parts/header.php'; ?>
             </div>
         </section>
 
-        <section id="schema">
-            <div class="container">
-                <img src="#" alt="schema-security">
+
+
+
+
+
+
+
+
+
+            <div class="title container text-center">
+                <h1 class="title-time">Vertical Timeline</h1>
+                <p>Sava Lazic</p>
             </div>
+
+        <section class="timeline">
+
+            <div class="container">
+
+                <div class="timeline-item">
+
+                    <div class="timeline-img"></div>
+
+                    <div class="timeline-content js--fadeInLeft">
+
+                        <h2>Title</h2>
+
+                        <div class="date">1 MAY 2016</div>
+
+                        <p class="explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias
+                            cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+                            excepturi amet in dolores. Alias, ullam.</p>
+
+                        <a class="bouton bnt-more" href="javascript:void(0)">More</a>
+
+                    </div>
+
+                </div>
+
+                <div class="timeline-item">
+
+                    <div class="timeline-img"></div>
+
+                    <div class="timeline-content timeline-card js--fadeInRight">
+
+                        <div class="timeline-img-header">
+
+                            <h2>Card Title</h2>
+
+                        </div>
+
+                        <div class="date">25 MAY 2016</div>
+
+                        <p class="explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias
+                            cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+                            excepturi amet in dolores. Alias, ullam.</p>
+
+                        <a class="bouton bnt-more" href="javascript:void(0)">More</a>
+
+                    </div>
+
+                </div>
+
+                <div class="timeline-item">
+
+                    <div class="timeline-img"></div>
+
+                    <div class="timeline-content timeline-card js--fadeInLeft">
+
+                        <div class="timeline-img-header">
+
+                            <h2>Card Title</h2>
+
+                        </div>
+
+                        <div class="date">10 JULY 2016</div>
+
+                        <p class="explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias
+                            cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+                            excepturi amet in dolores. Alias, ullam.</p>
+
+                        <a class="bouton bnt-more" href="javascript:void(0)">More</a>
+
+                    </div>
+
+                </div>
+
+                <div class="timeline-item">
+
+                    <div class="timeline-img"></div>
+
+                    <div class="timeline-content timeline-card js--fadeInRight">
+
+                        <div class="timeline-img-header">
+
+                            <h2>Card Title</h2>
+                        </div>
+
+                        <div class="date">30 JULY 2016</div>
+
+                        <p class="explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias
+                            cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+                            excepturi amet in dolores. Alias, ullam.</p>
+
+                        <a class="bouton bnt-more" href="javascript:void(0)">More</a>
+                    </div>
+
+                </div>
+
+                <div class="timeline-item">
+
+                    <div class="timeline-img"></div>
+
+                    <div class="timeline-content js--fadeInLeft">
+
+                        <div class="date">5 AUG 2016</div>
+
+                        <h2>Quote</h2>
+
+                        <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis
+                            omnis dolor iste fugit totam quasi inventore!
+
+                        </blockquote>
+
+                    </div>
+
+                </div>
+
+                <div class="timeline-item">
+
+
+                    <div class="timeline-img"></div>
+
+
+                    <div class="timeline-content timeline-card js--fadeInRight">
+
+                        <div class="timeline-img-header">
+
+                            <h2>Card Title</h2>
+
+                        </div>
+
+                        <div class="date">19 AUG 2016</div>
+
+                        <p class="Explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias
+                            cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+                            excepturi amet in dolores. Alias, ullam.</p>
+
+                        <a class="bouton bnt-more" href="javascript:void(0)">More</a>
+
+                    </div>
+
+                </div>
+
+                <div class="timeline-item">
+
+                    <div class="timeline-img"></div>
+
+                    <div class="timeline-content js--fadeInLeft">
+
+                        <div class="date">1 SEP 2016</div>
+
+                        <h2>Title</h2>
+
+                        <p class="Explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias
+                            cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+                            excepturi amet in dolores. Alias, ullam.</p>
+
+                        <a class="bouton bnt-more" href="javascript:void(0)">More</a>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
         </section>
+
+
+
 
     </main>
 
+    <script src="src/js/scrollreaveal.js"></script>
 
 <?php require_once 'template-parts/footer.php'; ?>
