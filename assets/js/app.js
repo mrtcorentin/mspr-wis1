f89902025cbe6766e0c6e8c0d907bf24ab7b92c7
Cabus <coline.cabuspro@gmail.com>
2021-02-04 11:13:36 +0100

    fix: timeline

--- assets/js/app.js
+++ assets/js/app.js
@@ -1395,7 +1395,9 @@ function isDomNode(x) {
   \***************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n   ╷\n15 │ @import \"pages/s'inscrire\";\r\n   │                           ^\n   ╵\n  C:\\wamp64\\www\\mspr-wis1\\src\\scss\\app.scss 15:27  root stylesheet\n    at processResult (C:\\wamp64\\www\\mspr-wis1\\node_modules\\webpack\\lib\\NormalModule.js:597:19)\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\webpack\\lib\\NormalModule.js:691:5\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\wamp64\\www\\mspr-wis1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:91024:16)\n    at _render_closure1.call$2 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:79938:12)\n    at _RootZone.runBinary$3$3 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:27187:18)\n    at _FutureListener.handleError$1 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:25715:19)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
 
 
 /***/ }),
