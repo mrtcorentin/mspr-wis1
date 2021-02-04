0d7db55ee3aa0ba38bd0cbdad9abcf4bd3c2ad33
Dorian Candy <cdy.dorian@gmail.com>
2021-02-04 12:09:31 +0100

    TA mere la pute

--- src/js/scrollreaveal.js
+++
@@ -1,8 +0,0 @@
-import ScrollReveal from "scrollreveal";
-
-ScrollReveal().reveal('.timeline-item', {
-    distance: '150%',
-    origin: 'bottom',
-    opacity: 0,
-    interval: 200,
-});
---
+++ template-parts/db.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$partners = [
+    ['name' => 'Armand Femme',      'picture' => 'armandf.png',         'categories' => ['vetements-femme']],
+    ['name' => 'Armand Homme',      'picture' => 'armandh.png',         'categories' => ['vetements-homme']],
+    ['name' => 'Au Pain Gourmand',  'picture' => 'aupaingourmand.png',  'categories' => ['alimentaire']],
+    ['name' => 'Bershka',           'picture' => 'bershka.jpg',         'categories' => ['vetements-femme']],
+    ['name' => 'Bio-coop',          'picture' => 'bio-coop.png',        'categories' => ['alimentaire']],
+    ['name' => 'Carrefour City',    'picture' => 'carrefour-city.png',  'categories' => ['alimentaire']],
+    ['name' => 'Celio',             'picture' => 'celio.jpg',           'categories' => ['vetements-homme']],
+    ['name' => 'Cop-Copine',        'picture' => 'cop-copine.png',      'categories' => ['vetements-femme']],
+    ['name' => 'Courir',            'picture' => 'courir.jpg',          'categories' => ['vetements-homme', 'vetements-femme']],
+    ['name' => 'Guess',             'picture' => 'guess.jpg',           'categories' => ['vetements-homme', 'vetements-femme']],
+    ['name' => 'Julie-Laurent',     'picture' => 'julie-laurent.jpg',   'categories' => ['vetements-femme']],
+    ['name' => 'Kaporal',           'picture' => 'kaporal.png',         'categories' => ['vetements-homme', 'vetements-femme']],
+    ['name' => 'Lidl',              'picture' => 'lidl.png',            'categories' => ['alimentaire']],
+    ['name' => 'M&O',               'picture' => 'm&o.png',             'categories' => ['alimentaire']],
+    ['name' => 'Mango',             'picture' => 'mango.png',           'categories' => ['vetements-homme', 'vetements-femme']],
+    ['name' => 'Naf-naf',           'picture' => 'naf-naf.jpg',         'categories' => ['vetements-femme']],
+    ['name' => 'Olly-Gan',          'picture' => 'olly-gan.jpg',        'categories' => ['vetements-homme']],
+    ['name' => 'Pharmacie',         'picture' => 'pharmacie.jpg',       'categories' => ['soins']],
+    ['name' => 'Pimkie',            'picture' => 'pimkie.jpg',          'categories' => ['vetements-femme']],
+    ['name' => 'Quater-Back',       'picture' => 'quater-back.jpg',     'categories' => ['vetements-homme']],
+    ['name' => 'Récré',             'picture' => 'recre',               'categories' => ['autres']],
+    ['name' => 'Spar',              'picture' => 'spar.jpg',            'categories' => ['alimentaire']],
+    ['name' => 'SpringField',       'picture' => 'springfield.jpg',     'categories' => ['vetements-homme', 'vetements-femme']],
+    ['name' => 'StreetShop',        'picture' => 'streetshop-cbd.jpg',  'categories' => ['soins']],
+    ['name' => 'Undiz',             'picture' => 'undiz.png',           'categories' => ['vetements-homme', 'vetements-femme']],
+    ['name' => 'Valège',            'picture' => 'valege.jpg',          'categories' => ['vetements-femme']],
+
+];
